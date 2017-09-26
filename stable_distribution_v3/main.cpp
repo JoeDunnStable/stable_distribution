9edb2498e46a3c4e82a2c3ffcf5087964c7153ff
JLDActuary <jldunn@verizon.net>
2017-09-25 23:42:43 -0400

    Initial Commit

---
+++ stable_distribution_v3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stable_distribution_v3
+//
+//  Created by Joseph Dunn on 9/25/17.
+//  Copyright © 2017 Joseph Dunn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
